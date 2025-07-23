6b4666f23d2ee04f15621eefa09cc329d2054abd
Luiz Felipe <lepinholima@gmail.com>
2025-07-23 15:20:49 -0300

    feat: create Shortening model and factory

---
+++ tests/Unit/Models/ShorteningTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\Shortening;
+
+it('has a factory', function (): void {
+    $shortening = Shortening::factory()->create()->refresh();
+    expect($shortening->id)->toBe(1);
+});
+
+test('to array', function (): void {
+    $shortening = Shortening::factory()->create()->refresh();
+    expect(array_keys($shortening->toArray()))->toEqual([
+        'id',
+        'full_url',
+        'hash',
+        'created_at',
+        'updated_at',
+    ]);
+});
